b3e631a87da0cc44dfdb39f9d11a5c75be185727
Keaton Smith <keaton.smith365@gmail.com>
2022-12-12 19:23:10 -0700

    Organized and added documentation

--- app/src/main/java/com/smith/timer/MainActivity.kt
+++ app/src/main/java/com/smith/timer/MainActivity.kt
@@ -15,116 +15,184 @@ import android.widget.Spinner
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var projectSelector : Spinner
-    lateinit var projectTitle : TextView
-    lateinit var displayTime : Chronometer
-    lateinit var newProjectTitle : EditText
+    // Components
+    private lateinit var projectSpinner : Spinner
+    private lateinit var tvActiveProject : TextView
+    private lateinit var projectChronometer : Chronometer
+    private lateinit var inputNewProject : EditText
 
-    lateinit var projectPrefs : SharedPreferences
-    lateinit var projectEditor : SharedPreferences.Editor
+    // Shared Preferences
+    private lateinit var projectsFile : SharedPreferences
+    private lateinit var projectsEditor : SharedPreferences.Editor
 
+    // State Variables
     private var activeProject : String? = null
+    private var elapsedTime : Long = 0
+    private var running = false
 
-    var elapsedTime : Long = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    var running = false
+        // Setup Components
+        projectSpinner = findViewById(R.id.sSelector)
+
+        tvActiveProject = findViewById(R.id.tvActiveProject)
+        projectChronometer = findViewById(R.id.cmTimeSpent)
+        val buttonStart = findViewById<Button>(R.id.bStart)
+        val buttonStop = findViewById<Button>(R.id.bStop)
+
+        inputNewProject = findViewById(R.id.etTitleInput)
+        val buttonCreate = findViewById<Button>(R.id.bCreate)
+
+        // Set up Shared Preferences
+        projectsFile = getSharedPreferences("projects", Context.MODE_PRIVATE)
+        projectsEditor = projectsFile.edit()
+
+        // Used for testing
+//        projectsEditor.clear()
+//        projectsEditor.commit()
+
+        // Assume there are no existing projects
+        // This will be overwritten if the projectSpinner gets populated
+        tvActiveProject.text = "Create project below"
+
+        // projectChronometer does not require any setup here
+        // inputNewProject does not require any setup here
+
+        // Setup Buttons
+        buttonStart.setOnClickListener { startTimer() }
+        buttonStop.setOnClickListener { stopTimer() }
+
+        buttonCreate.setOnClickListener { createProject() }
+
+        // Setup Spinner
+        projectSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                setActiveProject(projectSpinner.selectedItem as String)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // TODO Handle the case when activeProject is deleted.
+            }
+        }
+
+        populateProjectSpinner()
+    }
 
     override fun onDestroy() {
         super.onDestroy()
 
+        // Save project
         saveActiveProject()
     }
 
     override fun onPause() {
         super.onPause()
 
+        // Save project
         saveActiveProject()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        projectSelector = findViewById(R.id.sSelector)
-
-        projectTitle = findViewById(R.id.tvActiveProject)
-        displayTime = findViewById(R.id.cmTimeSpent)
-
-        newProjectTitle = findViewById(R.id.etTitleInput)
-
-        val buttonStart = findViewById<Button>(R.id.bStart)
-        val buttonStop = findViewById<Button>(R.id.bStop)
-
-        val buttonCreate = findViewById<Button>(R.id.bCreate)
-
-
-        projectPrefs = getSharedPreferences("projects", Context.MODE_PRIVATE)
-        projectEditor = projectPrefs.edit()
-
-        // Used for testing
-//        projectEditor.clear()
-//        projectEditor.commit()
-
-        updateProjectOptions()
-
-        val project = getProjects().keys.elementAtOrElse(0, {null})
-        if (project != null) setActiveProject(project)
-
-
-        projectTitle.text = if (activeProject != null) activeProject else "Create project below"
-
-        projectSelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                setActiveProject(projectSelector.selectedItem as String)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                println("bye")
-            }
-        }
-
-        buttonStart.setOnClickListener { startTimer() }
-        buttonStop.setOnClickListener { stopTimer() }
-        buttonCreate.setOnClickListener { createProject() }
-
-
-    }
-
+    /**
+     * Start the timer
+     */
     private fun startTimer() {
+        // Only start if the timer is not running and a project is selected
         if (!running && activeProject != null) {
+            // Change the chronometer base appropriately
             updateDisplayTime()
-            displayTime.start()
+            // Start
+            projectChronometer.start()
             running = true
         }
     }
 
+    /**
+     * Stop the timer
+     */
     private fun stopTimer() {
+        // Only stop if not running
         if (running) {
+            // Calculate elapsed time
             val stopTime = SystemClock.elapsedRealtime()
-            displayTime.stop()
-            val startTime = displayTime.getBase()
+            val startTime = projectChronometer.getBase()
             elapsedTime = stopTime - startTime
+            // Stop
+            projectChronometer.stop()
             running = false
         }
     }
 
-    private fun createProject() {
-        val project = newProjectTitle.text.toString()
-        if (project == "") return
-        stopTimer()
-
-        projectEditor.putLong(project, 0)
-        projectEditor.commit()
-
-        updateProjectOptions()
-
-        // Spinner should select this project now
-        projectSelector.setSelection(getProjects().keys.toList().indexOf(project))
-
-        newProjectTitle.text.clear()
+    /**
+     * Update chronometer base
+     */
+    private fun updateDisplayTime() {
+        projectChronometer.setBase(SystemClock.elapsedRealtime() - elapsedTime)
     }
 
-    private fun updateProjectOptions() {
+    /**
+     * Create a new project and make it active
+     */
+    private fun createProject() {
+        val project = inputNewProject.text.toString()
+
+        if (project == "") return
+
+        // Save the new project
+        projectsEditor.putLong(project, 0)
+        projectsEditor.commit()
+
+        // Repopulate spinner to include new value
+        populateProjectSpinner()
+
+        // Spinner should now select the new project
+        projectSpinner.setSelection(getProjects().keys.toList().indexOf(project))
+
+        // Clear input field
+        inputNewProject.text.clear()
+    }
+
+    /**
+     * Configure timer for selected project
+     */
+    private fun setActiveProject(project : String?) {
+        // Stop and Save the current project to preference file
+        stopTimer()
+        saveActiveProject()
+
+        // Switch to project
+        activeProject = project
+        elapsedTime = getProjects().get(activeProject)!!
+
+        // Update display
+        tvActiveProject.text = activeProject
+        updateDisplayTime()
+    }
+
+    /**
+     * Save selected project to Shared Preferences
+     */
+    private fun saveActiveProject() {
+        if (activeProject == null) return
+
+        // If the timer is running, calculate elapsedTime. Otherwise used pre-calculated value
+        val projectTime =
+            if (running)
+                SystemClock.elapsedRealtime() - projectChronometer.getBase()
+            else
+                elapsedTime
+
+        // Save to Shared Preferences
+        projectsEditor.putLong(activeProject, projectTime)
+        projectsEditor.commit()
+    }
+
+    /**
+     * Populate projectSpinner with all existing projects
+     */
+    private fun populateProjectSpinner() {
+        // Get all existing projects
         val projects = getProjects()
 
         // Populate projectSelector
@@ -134,41 +202,15 @@ class MainActivity : AppCompatActivity() {
             projects.keys.toMutableList()
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            projectSelector.adapter = adapter
+            // Apply adapter
+            projectSpinner.adapter = adapter
         }
     }
 
-    private fun setActiveProject(project : String?) {
-        // Save the current project to preference file
-        saveActiveProject()
-
-        // Switch to project
-        activeProject = project
-        elapsedTime = getProjects().get(activeProject)!!
-
-        // Update display
-        projectTitle.text = activeProject
-        updateDisplayTime()
-    }
-
-    private fun saveActiveProject() {
-        if (activeProject == null) return
-
-        val projectTime =
-            if (running)
-                SystemClock.elapsedRealtime() - displayTime.getBase()
-            else
-                elapsedTime
-
-        projectEditor.putLong(activeProject, projectTime)
-        projectEditor.commit()
-    }
-
+    /**
+     * Get all existing projects
+     */
     private fun getProjects() : Map<String, Long> {
-        return projectPrefs.getAll() as Map<String, Long>
-    }
-
-    private fun updateDisplayTime() {
-        displayTime.setBase(SystemClock.elapsedRealtime() - elapsedTime)
+        return projectsFile.getAll() as Map<String, Long>
     }
 }
